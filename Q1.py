3b379d09bd61f5304b9d431226a275d4e3c702b5
Marcos Willian <115299182+mw-bl@users.noreply.github.com>
2023-04-24 16:02:09 -0300

    Create Q1.py

---
+++ Q1.py
@@ -0,0 +1,10 @@
+numeros = []
+
+for i in range(5):
+    numero = int(input("Digite um número: "))
+    numeros.append(numero)
+
+novoNumero = int(input("Digite outro número: "))
+numeros.append(novoNumero)
+
+print("Lista de números:", numeros)
